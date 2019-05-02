869123712396053b6b40b269a0703205424cce54
Chris Ball <chrisball7@gmail.com>
2019-05-02 13:58:02 -0700

    working on getting static files to register

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bsls4_q03p#c)l#p6wm+ll1m@d9=jq6-9&up#h#t9uh0*5so@2'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
